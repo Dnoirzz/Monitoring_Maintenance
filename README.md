c90972960405a5bde67ff6f2dc69f5dd25c43d04
Adityo <adityodwisaputro007@gmail.com>
2025-11-15 10:18:57 +0700

    Menambahkan Dashboard Admin

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # monitoring_maintenance
 
-A new Flutter project.
+🏭 Aplikasi Monitoring Maintenance Mesin Pabrik Karet
+📌 Deskripsi Singkat
+
+Aplikasi ini dibuat untuk membantu proses monitoring jadwal maintenance mesin di pabrik karet.
+Dengan sistem ini, pihak teknisi dan kepala teknisi dapat memantau status mesin, mencatat kegiatan perawatan, serta menerima notifikasi otomatis H-7 sebelum jadwal maintenance berikutnya.
+Tujuan utamanya adalah meminimalkan keterlambatan perawatan dan mengurangi risiko kerusakan mendadak (breakdown). Di bagian dashboard digunakan untuk menampilkan daftar mesin yang perlu dimaintenance (yang terjadwal memiliki status "perlu dimaintenance" secara otomatis mengikuti jadwal maintenance dan yang tidak terjadwal memiliki status "breakdown" yang diatur sendiri oleh teknisi dan kepala teknisi)
+
+🚀 Fitur Utama
+1. Manajemen User
+a. Tiga jenis role: Teknisi, Kepala Teknisi, Admin
+b. Login, autentikasi, dan hak akses berdasarkan peran
+
+2. Data Mesin
+a. Menyimpan informasi mesin, kode aset, dan status (Active, Breakdown, Perlu Maintenance)
+b. Jadwal Maintenance
+c. Mencatat jadwal perawatan terakhir dan berikutnya
+d. Cek harian mesin yg menampilkan nama mesin, bagian mesin berupa dropdown, jenis pekerjaan text input, catatan, tanggal, bukti foto, dan nama petugas.
+
+3. Notifikasi Otomatis
+a. Mengingatkan user 7 hari sebelum tanggal maintenance berikutnya
+b. Ditampilkan di halaman dashboard dan memunculkan popup dari luar aplikasi
+
+4. Menu Riwayat
+a. Menampilkan riwayat maintenance mesin berdasarkan periode waktu atau status
+
+5. Multi-Lokasi
+a. Setiap user dan mesin terhubung ke lokasi tertentu di pabrik
 
 ## Getting Started
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,143 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:monitoring_maintenance/screen/dashboard_admin.dart';
+// import 'screen/splach_scr.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: KomponenPage(),
-  ));
+  runApp(const MyApp());
 }
 
-class KomponenPage extends StatelessWidget {
-  const KomponenPage({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.all(20),
-        children: const [
-          Text('Nomor Aset:-', style: TextStyle(fontSize: 16)),
-          SizedBox(height: 6),
-          Text('Bagian Mesin', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          SizedBox(height: 20),
-          ItemPanel(
-            title: 'Roll Atas',
-            data: [
-              ['Bearing', 'SKF 6203'],
-              ['Shaft', 'Stainless Ø20'],
-              ['Baut Penahan', 'M8 x 25'],
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ItemPanel extends StatefulWidget {
-  final String title;
-  final List<List<String>> data;
-
-  const ItemPanel({
-    super.key,
-    required this.title,
-    required this.data,
-  });
-
-  @override
-  State<ItemPanel> createState() => _ItemPanelState();
-}
-
-class _ItemPanelState extends State<ItemPanel> {
-  late List<List<String>> rows;
-
-  @override
-  void initState() {
-    super.initState();
-    rows = widget.data.map((e) => [...e]).toList();
-  }
-
-  void editRow(int index) {
-    final controller = TextEditingController(text: rows[index][1]);
-
-    showDialog(
-      context: context,
-      builder: (_) {
-        return AlertDialog(
-          title: const Text('Ubah Komponen'),
-          content: TextField(
-            controller: controller,
-            decoration: const InputDecoration(labelText: 'Komponen Digunakan'),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  rows[index][1] = controller.text.trim();
-                });
-                Navigator.pop(context);
-              },
-              child: const Text('SIMPAN'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ExpansionTile(
-      tilePadding: EdgeInsets.zero,
-      childrenPadding: const EdgeInsets.only(bottom: 10),
-      title: Text(widget.title, style: const TextStyle(fontSize: 16)),
-      children: [
-        Table(
-          border: TableBorder.all(color: Colors.black54, width: 1),
-          columnWidths: const {
-            0: FlexColumnWidth(2),
-            1: FlexColumnWidth(3),
-            2: FlexColumnWidth(1),
-          },
-          children: [
-            TableRow(
-              decoration: BoxDecoration(color: Colors.grey.shade200),
-              children: const [
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Text('Bagian Komponen', style: TextStyle(fontWeight: FontWeight.bold)),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Text('Komponen Digunakan', style: TextStyle(fontWeight: FontWeight.bold)),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Text('Edit', style: TextStyle(fontWeight: FontWeight.bold)),
-                ),
-              ],
-            ),
-            ...List.generate(rows.length, (i) {
-              return TableRow(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Text(rows[i][0]),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Text(rows[i][1]),
-                  ),
-                  IconButton(
-                    onPressed: () => editRow(i),
-                    icon: const Icon(Icons.edit, size: 20),
-                  )
-                ],
-              );
-            }),
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aplikasi Monitoring Maintenance Mesin',
+      theme: ThemeData(
+        fontFamily: 'Arial',
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFF0A9C5D),
+          secondary: Color(0xFF022415),
         ),
-      ],
+        useMaterial3: true,
+      ),
+      home: AdminTemplate(),
     );
   }
 }
